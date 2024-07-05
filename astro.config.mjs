86e4b394de946650c808b1ee94b0b373e45552bd
Noah <duyngo@panicbear.consulting>
2024-07-05 11:59:58 +0700

    Update serverless vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import icon from 'astro-icon';
 import compress from 'astro-compress';
 import vercel from '@astrojs/vercel/serverless';
 
+
 // https://astro.build/config
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
