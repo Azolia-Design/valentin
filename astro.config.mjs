ca8e2724cb7c6bf447b0655877b4bf0108893b29
Noah <duyngo@panicbear.consulting>
2024-07-05 11:34:10 +0700

    Update infinite scroll

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ const whenExternalScripts = (items = []) =>
     hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-    output: 'hybrid',
+    output: 'static',
     devToolbar: {
         enabled: false
     },
