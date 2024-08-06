35b36e82dcecc7bd3149f68116d0d933b2b4710f
Noah <duyngo@panicbear.consulting>
2024-08-06 14:28:21 +0700

    Update Hero

--- src/components/pages/Home/Hero/script.jsx
+++ src/components/pages/Home/Hero/script.jsx
@@ -2,8 +2,6 @@ import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { onMount, onCleanup } from 'solid-js';
 import { loadImage, loadImages } from '~/utils/loadImage';
-import FULLTILT from '~/vendors/fulltilt';
-import GyroNorm from 'gyronorm';
 import { clamp } from '~/utils/number';
 
 const HeroScript = (props) => {
@@ -23,11 +21,11 @@ const HeroScript = (props) => {
         })
         tl
             .fromTo('.home__hero-bg-main-inner', { scale: 1, xPercent: 0, yPercent: 0 }, { scale: 1.7, xPercent: 5, yPercent: -8, duration: 2, ease: 'linear' })
-            .fromTo('.home__hero-title-wrap', { autoAlpha: 1 }, { autoAlpha: 1, duration: .5, ease: 'linear' }, 0)
+            .fromTo(['.home__hero-title-wrap', '.home__hero-scope-wrap', '.home__hero-intro-wrap'], { autoAlpha: 1 }, { autoAlpha: 0, stagger: .1, duration: .5, ease: 'linear' }, 0)
             .fromTo('.home__hero-bg-main-inner', { autoAlpha: 1 }, { autoAlpha: 0, duration: 1, ease: 'linear' }, '>.5')
             // .fromTo(['.home__hero-clone-wrap', '.home__hero-bg-main-inner'], { autoAlpha: 1 }, { autoAlpha: 0, duration: 1, ease: 'linear' }, '>.5')
             // .fromTo('.home__intro-bg-gradient', { autoAlpha: 0 }, { autoAlpha: 1, duration: 1, ease: 'linear' }, '-=1')
-            .fromTo('.home__intro-companies', { yPercent: 0 }, { yPercent: 20, duration: 1, ease: 'linear' }, "<.3")
+            .fromTo('.home__intro-companies', { yPercent: 0 }, { yPercent: window.innerWidth > 767 ? 20 : 0, duration: 1, ease: 'linear' }, "<.3")
             .to('.home__intro-bg-gradient', { display: 'none', duration: 0, ease: 'linear' });
 
         const vertex = `
@@ -68,8 +66,9 @@ const HeroScript = (props) => {
             }
         `;
 
-        let gn = new GyroNorm();
-        gn.FULLTILT = FULLTILT;
+        const gn = new GyroNorm();
+
+        // gn.FULLTILT = FULLTILT;
         class Sketch {
             constructor(parent) {
                 this.container = document.getElementById(parent);
