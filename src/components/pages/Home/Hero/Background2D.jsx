c8a8dfac49bfd5ce1c184a37af6e1538ca6eb6be
Noah <duyngo@panicbear.consulting>
2024-08-09 15:26:18 +0700

    Update Hero Optimized

--- src/components/pages/Home/Hero/Background2D.jsx
+++ src/components/pages/Home/Hero/Background2D.jsx
@@ -16,32 +16,32 @@ const vertex = `
     }
 `
 const fragment = `
-    #ifdef GL_ES
     precision mediump float;
-    #endif
 
     uniform vec4 resolution;
     uniform vec2 mouse;
     uniform vec2 threshold;
-    uniform float time;
     uniform float pixelRatio;
     uniform sampler2D image0;
     uniform sampler2D image1;
 
     vec2 mirrored(vec2 v) {
-        vec2 m = mod(v,2.);
-        return mix(m,2.0 - m, step(1.0 ,m));
+        vec2 m = mod(v, 2.0);
+        return mix(m, 2.0 - m, step(1.0, m));
     }
 
     void main() {
-        // uvs and textures
-        vec2 uv = pixelRatio*gl_FragCoord.xy / resolution.xy ;
-        vec2 vUv = (uv - vec2(.5, 1.0))*resolution.zw + vec2(0.5, 1.0);
+        // Calculate UV coordinates and apply pixel ratio
+        vec2 uv = gl_FragCoord.xy * pixelRatio / resolution.xy;
+        vec2 vUv = (uv - vec2(0.5, 1.0)) * resolution.zw + vec2(0.5, 1.0);
         vUv.y = 1.0 - vUv.y;
 
-        vec4 tex1 = texture2D(image1,mirrored(vUv));
-        vec2 fake3d = vec2(vUv.x + (tex1.r - 0.5)*mouse.x/threshold.x, vUv.y + (tex1.r - 0.5)*mouse.y/threshold.y);
+        // Sample the depth map
+        vec4 tex1 = texture2D(image1, mirrored(vUv));
+        // Apply the fake 3D effect using depth map and mouse position
+        vec2 fake3d = vUv + (tex1.r - 0.5) * mouse / threshold;
 
+        // Sample the final texture using the modified UV coordinates
         gl_FragColor = texture2D(image0, mirrored(fake3d));
     }
 `;
