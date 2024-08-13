fc9c9e28af8560491f296247e59c590b1618ccf6
Noah <duyngo@panicbear.consulting>
2024-08-13 14:46:52 +0700

    Update Hero Optimized

--- src/components/core/lenis.js
+++ src/components/core/lenis.js
@@ -3,14 +3,11 @@ import Lenis from 'lenis';
 let lenis;
 
 function initLenis() {
-    console.log(typeof document.querySelector('.wrapper'))
-    console.log(document.querySelector('.wrapper'))
     lenis = new Lenis({
         // infinite: true
         content: window.innerWidth > 767 ? document.documentElement : document.querySelector('.wrapper'),
         wrapper: window.innerWidth > 767 ? document.documentElement : document.querySelector('.wrapper')
     });
-    console.log(lenis)
 
     function raf(time) {
         lenis.raf(time)
