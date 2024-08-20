4bd1f41d0c47da6b6a42df7718a8746d786c124f
Noah <duyngo@panicbear.consulting>
2024-08-20 14:12:26 +0700

    Update touch lenis

--- src/components/core/lenis.js
+++ src/components/core/lenis.js
@@ -5,11 +5,6 @@ let lenis;
 function initLenis(options = {}) {
     lenis = new Lenis({
         infinite: true,
-        syncTouch: true,
-        smooth: true,
-        smoothTouch: true,
-        touchInertiaMultiplier: 5,
-        touchMultiplier: 2,
         content: window.innerWidth > 767 ? document.documentElement : document.querySelector('.wrapper'),
         wrapper: window.innerWidth > 767 ? document.documentElement : document.querySelector('.wrapper'),
         ...options
